a5533b659238e0f96712db732831207b85c1b9d9
Tobias von Arx <tvonarx@student.ethz.ch>
2021-12-02 10:42:36 +0100

    fix

--- 1/golf.py
+++ 1/golf.py
@@ -1,1 +1,1 @@
-print(sum([j := 2, x := [int(h) for h in open('i').read().split('\n')], [1 for i in range(1+j,len(x)) if sum(x[i-1-j:i]) < sum(x[i-j:i+1])]][2]))
+print(sum([j := 3, x := [int(h) for h in open('i').read().split('\n')], [1 for i in range(j,len(x)) if x[i-j] < x[i]]][2]))
