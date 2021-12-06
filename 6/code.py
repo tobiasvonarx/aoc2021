8b7790ffa2a06f6a6cdda155342ed795a419adeb
= <tobiasvonarx1@gmail.com>
2021-12-06 15:37:02 +0100

    d6

--- 6/code.py
+++ 6/code.py
@@ -8,12 +8,13 @@ if len(sys.argv) > 1:
     FILENAME = sys.argv[1]
 
 with open(FILENAME, 'r') as input_file:
-    inp = input_file.read().strip().split('\n')
+    inp = list(map(int, input_file.read().strip().split(',')))
 
-for line in inp:
-    pass
+x = [inp.count(i) for i in range(9)]
 
+for _ in range(256):
+    n = x.pop(0)
+    x[6] += n
+    x.append(n)
 
-
-
-
+print(sum(x))
