b8a808582bfcf94ea1fea4b278df4b773c73607a
Tobias von Arx <tvonarx@student.ethz.ch>
2021-12-08 07:47:33 +0100

    7

--- 7/code.py
+++ 7/code.py
@@ -8,12 +8,26 @@ if len(sys.argv) > 1:
     FILENAME = sys.argv[1]
 
 with open(FILENAME, 'r') as input_file:
-    inp = input_file.read().strip().split('\n')
+    inp = input_file.read().strip()
 
-for line in inp:
-    pass
+z = 100000000000000000000000000000000000
+res = -1
 
+x = [int(i) for i in inp.split(',')]
 
+for y in range(max(x)+1):
+    s = 0
+    for i in x:
+        d = abs(i - y)
+        for j in range(d):
+            d += j
 
+        # dist per fuel
+        s += d
+    if s < z:
+        z = s
+        res = s
+
+print(z)
 
 
