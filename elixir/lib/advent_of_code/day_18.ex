faafa4f17d3fe766520e92c2da729e7bfd836f9a
= <tobiasvonarx1@gmail.com>
2021-12-28 16:26:46 +0100

    18&19

--- elixir/lib/advent_of_code/day_18.ex
+++ elixir/lib/advent_of_code/day_18.ex
@@ -1,7 +1,122 @@
 defmodule AdventOfCode.Day18 do
+  def parse(inp) do
+    inp
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn s -> s |> Code.eval_string([], __ENV__) |> elem(0) end)
+  end
+
+  def reduce(num) do
+    {exploded, num, _, _} = explode(num, 0)
+    if exploded do
+      # reduce until we no longer explode
+      reduce(num)
+    else
+      # try to split
+      {splt, num} = split(num)
+
+      if splt do
+        # repeat
+        reduce(num)
+      else
+        # reducing done
+        num
+      end
+    end
+  end
+
+  # try to find the leftmost 4x nested number and then return
+  # {true, sub-expr to replace with, left increase, right increase}
+  
+  # depth 4 found, explode
+  # depth > 4 can't happen, because after adding two reduced numbers we can get depth 4 max
+  def explode([l, r], 4), do: {true, 0, l, r}
+  # depth less than 4
+  def explode(num, _) when is_number(num), do: {false, num, 0, 0}
+
+  def explode([l, r], d) do
+    # search left first
+    {exploded, num, l_inc, r_inc} = explode(l, d+1)
+
+    #IO.write("num: ")
+    #IO.inspect([l, r])
+
+    if exploded do
+      #IO.puts("left side did explode")
+      #exit(:shutdown)
+      # add to the right side
+      {true, [num, right_add(r, r_inc)], l_inc, 0}
+    else
+      {exploded, num, l_inc, r_inc} = explode(r, d+1)
+      if exploded do
+        #IO.puts("right side did explode")
+        # add to the left side
+        {true, [left_add(l, l_inc), num], 0, r_inc}
+      else
+        #IO.puts("right side didn't explode")
+        {false, [l, r], l_inc, r_inc}
+      end
+    end
+  end
+
+  # add to_add to the immediately left number
+  def left_add([l, r], to_add), do: [l, left_add(r, to_add)]
+  def left_add(num, to_add) when is_number(num), do: num + to_add
+
+  # add to_add to the immediately right number
+  def right_add([l, r], to_add), do: [right_add(l, to_add), r]
+  def right_add(num, to_add) when is_number(num), do: num + to_add
+
+  # recursively try to split
+  def split([l, r]) do
+    {splt, l} = split(l)
+    if splt do
+      {true, [l, r]}
+    else
+      {splt, r} = split(r)
+      if splt do
+        {true, [l, r]}
+      else
+        {false, [l, r]}
+      end
+    end
+  end
+  
+
+  # {did it split, replace with subexpr}
+  def split(num) when num < 10, do: {false, num}
+  def split(num), do: {true, [floor(num/2), ceil(num/2)]}
+
+  def magnitude([l, r]), do: 3*magnitude(l) + 2*magnitude(r)
+  def magnitude(num), do: num
+
   def part1(args) do
+    args
+    |> parse()
+    |> Enum.reduce([],
+      fn num, acc ->
+        if acc == [] do
+          num
+        else
+          reduce([acc, num])
+        end
+      end
+    )
+    |> magnitude()
+
+  end
+
+  def comb(nums) do
+    for a <- nums, b <- nums, a != b do
+      # add a and b
+      reduce([a, b]) |> magnitude()
+    end
   end
 
   def part2(args) do
+    args
+    |> parse()
+    |> comb()
+    |> Enum.max()
   end
 end
--- elixir/lib/advent_of_code/day_19.ex
+++ elixir/lib/advent_of_code/day_19.ex
@@ -1,7 +1,111 @@
 defmodule AdventOfCode.Day19 do
-  def part1(args) do
+
+  @orientations [
+    [[1, 0, 0], [0, 1, 0], [0, 0, 1]],   # Unmodified
+    [[1, 0, 0], [0, 0, -1], [0, 1, 0]],  # 90
+    [[1, 0, 0], [0, -1, 0], [0, 0, -1]], # 180
+    [[1, 0, 0], [0, 0, 1], [0, -1, 0]],  # 270
+  ]
+
+  @rotations [
+    [[1, 0, 0], [0, 1, 0], [0, 0, 1]],   # Unmodified
+    [[0, 1, 0], [-1, 0, 0], [0, 0, 1]],  # z 90
+    [[-1, 0, 0], [0, -1, 0], [0, 0, 1]], # z 180
+    [[0, -1, 0], [1, 0, 0], [0, 0, 1]],  # z 270
+    [[0, 0, -1], [0, 1, 0], [1, 0, 0]],  # y 90
+    [[0, 0, 1], [0, 1, 0], [-1, 0, 0]],  # y 270
+    # y 180 is the same as z 180
+  ]
+
+  @translations (for rotation <- @rotations, orientation <- @orientations do
+    # Multiply rotation matrix with orientation matrix
+    transposed = orientation |> Enum.zip() |> Enum.map(&Tuple.to_list/1)
+    for row <- rotation do
+      for col <- transposed do
+        Enum.zip(row, col) |> Enum.map(fn {l, r} -> l * r end) |> Enum.sum()
+      end
+    end
+  end)
+
+
+  def merge_scanners([hd | tl]), do: merge_scanners(MapSet.new(hd), [[0,0,0]], tl)
+
+  def merge_scanners(merged, beacons, []), do: {merged, beacons}
+
+  def merge_scanners(merged_beacons, beacons, [scanner_beacons | scanners]) do
+    distances = distances(merged_beacons)
+    translations = all_translations(scanner_beacons)
+    all_distances = Enum.map(translations, fn {m, t} -> {m, distances(t)} end)
+
+    all_distances
+    |> Stream.map(fn {m, d} -> {m, matching_distances(d, distances)} end)
+    |> Enum.find(fn {_, d} -> map_size(d) >= 12 end)
+    |> case do
+      {transformation, matching_distances} ->
+        {distance, {scanner_beacon, _}} = matching_distances |> Enum.take(1) |> hd()
+        range_difference = distance(scanner_beacon, elem(distances[distance], 0))
+
+        scanner_beacons
+        |> Enum.map(&translate(transformation, &1))
+        |> Enum.map(&distance(&1, range_difference))
+        |> MapSet.new()
+        |> MapSet.union(merged_beacons)
+        |> merge_scanners([range_difference | beacons], scanners)
+
+      nil ->
+        merge_scanners(merged_beacons, beacons, scanners ++ [scanner_beacons])
+    end
   end
 
+  def all_translations(beacons) do
+    Enum.map(@translations, fn t -> {t, Enum.map(beacons, &translate(t, &1))} end)
+  end
+
+  def translate(m, v), do: Enum.map(m, &dot(&1, v))
+
+  def dot(v1, v2), do: Enum.zip(v1, v2) |> Enum.map(fn {l, r} -> l * r end) |> Enum.sum()
+
+  def matching_distances(d1, d2), do: Map.take(d1, Map.keys(d2))
+
+  def distances(beacons) do
+    beacons
+    |> Enum.flat_map(fn beacon -> beacons |> Enum.map(&{distance(beacon, &1), {beacon, &1}}) end)
+    |> Enum.reject(fn {_, {b1, b2}} -> b1 == b2 end)
+    |> Map.new()
+  end
+
+  def distance([lx, ly, lz], [rx, ry, rz]), do: [lx - rx, ly - ry, lz - rz]
+
+  def parse(string) do
+    string
+    |> String.trim()
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(&tl/1)
+    |> Enum.map(
+      fn scanner ->
+        scanner
+        |> Enum.map(
+          fn line ->
+            line
+            |> String.split(",")
+            |> Enum.map(&String.to_integer/1)
+          end
+        )
+      end
+    )
+  end
+
+  def part1(args), do: args |> parse() |> merge_scanners() |> elem(0) |> MapSet.size()
+
   def part2(args) do
+    args
+    |> parse()
+    |> merge_scanners()
+    |> elem(1)
+    |> distances()
+    |> Map.keys()
+    |> Enum.map(fn d -> d |> Enum.map(&abs/1) |> Enum.sum() end)
+    |> Enum.max()
   end
 end
